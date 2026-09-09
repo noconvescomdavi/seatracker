c5cca346e8d165f54ce605da7716419b7e0e4d5f
noconvescomdavi <davilol42@gmail.com>
2026-09-09 11:05:42 -0300

    build(android): version functional GPS chart route build

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.noconves.seatracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-tech-preview"
+        versionCode = 2
+        versionName = "0.2.0-functional"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
